834f75a61d79d0adc7389b2c310988c9d64b5aa6
Konstantin Antonov <koantonov99@gmail.com>
2022-03-23 20:16:53 +0700

    Add UI features, get rid of code duplication

--- app/src/main/java/com/example/inputform/MainActivity.kt
+++ app/src/main/java/com/example/inputform/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.inputform
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.Editable
+import android.text.SpannableString
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.inputform.databinding.ActivityMainBinding
-import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import java.util.regex.Pattern
 
 const val PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=." +
@@ -23,13 +27,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // todo somehow get rid of code duplication
-        binding.loginEditText.listenForChanges {
-            binding.loginInputLayout.isErrorEnabled = false
-        }
-        binding.passwordEditText.listenForChanges {
-            binding.passwordInputLayout.isErrorEnabled = false
-        }
+        listenForChanges(binding.loginEditText, binding.loginInputLayout)
+        listenForChanges(binding.passwordEditText, binding.passwordInputLayout)
+
         binding.loginButton.setOnClickListener {
             val isInputValid = processInputErrors(
                 binding.loginEditText.text.toString(),
@@ -39,6 +39,16 @@ class MainActivity : AppCompatActivity() {
                 onLoggedIn()
             }
         }
+
+        binding.checkbox.text = SpannableString(getString(R.string.agreement))
+        binding.loginButton.isEnabled = false
+        binding.checkbox.setOnCheckedChangeListener { _, isChecked ->
+            binding.loginButton.isEnabled = isChecked
+        }
+    }
+
+    private fun listenForChanges(editText: TextInputEditText, textInputLayout: TextInputLayout) {
+        editText.listenForChanges { textInputLayout.isErrorEnabled = false }
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
@@ -52,32 +62,43 @@ class MainActivity : AppCompatActivity() {
     private fun processInputErrors(login: String, password: String) : Boolean {
         Log.d(TAG, "processInputErrors called")
 
-        // How to move duplicate code to separate function
-        // if it is impossible to access binding.param,
-        // where param: TextInputLayout ?
         val isLoginValid = android.util.Patterns.EMAIL_ADDRESS.matcher(login).matches()
-        binding.loginInputLayout.isErrorEnabled = !isLoginValid
-        val loginError = if (isLoginValid) "" else getString(R.string.invalid_email)
-        binding.loginInputLayout.error = loginError
+        handleTextInputLayoutError(binding.loginInputLayout, isLoginValid, getString(R.string.invalid_email))
 
         val isPasswordValid = Pattern.compile(PASSWORD_PATTERN).matcher(password).matches()
-        binding.passwordInputLayout.isErrorEnabled = !isPasswordValid
-        val passwordError = if (isPasswordValid) "" else getString(R.string.invalid_password)
-        binding.passwordInputLayout.error = passwordError
+        handleTextInputLayoutError(binding.passwordInputLayout, isPasswordValid, getString(R.string.invalid_password))
 
         return isLoginValid && isPasswordValid
     }
 
-    private fun onLoggedIn() {
-        binding.loginButton.isEnabled = false
-
-        hideKeyboard(binding.loginEditText)
-        Snackbar.make(
-            binding.loginButton,
-            getString(R.string.logged_in),
-            Snackbar.LENGTH_SHORT
-        ).show()
+    private fun handleTextInputLayoutError(
+        textInputLayout: TextInputLayout,
+        isValid: Boolean,
+        errorText: String
+    ) {
+        textInputLayout.isErrorEnabled = !isValid
+        textInputLayout.error = if (isValid) "" else errorText
     }
+
+    private fun onLoggedIn() {
+        fun switchViewsVisibility() {
+            binding.loginInputLayout.isEnabled = !binding.loginInputLayout.isEnabled
+            binding.passwordInputLayout.isEnabled = !binding.passwordInputLayout.isEnabled
+            binding.checkbox.isEnabled = !binding.checkbox.isEnabled
+            binding.loginButton.isEnabled = !binding.loginButton.isEnabled
+            binding.progressBar.visibility = if (binding.progressBar.visibility == View.VISIBLE)
+                View.INVISIBLE else View.VISIBLE
+        }
+
+        switchViewsVisibility()
+        hideKeyboard(binding.loginEditText)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            AlertDialog.Builder(this).setMessage(R.string.service_unavailable).show()
+            switchViewsVisibility()
+        }, 3000)
+    }
+
 
     private fun TextInputEditText.listenForChanges(func: (text: String) -> Unit) {
         addTextChangedListener(object : SimpleTextWatcher() {
